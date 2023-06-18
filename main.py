7298a19be53c499dffccec17a4a5c61868b0d87b
d.larin <larin.dn@icloud.com>
2023-06-18 13:31:06 +0400

    cosmetic_1.4

--- main.py
+++ main.py
@@ -27,42 +27,43 @@ def main():
     while True:
         command = input(
             "\n------------------------------------\n"
-            "1 - Список всех компаний и количество вакансий у каждой компании; \n"
-            "2 - Список всех вакансий с указанием названия компании, названия вакансии, зарплаты и ссылки; \n"
-            "3 - Средняя зарплата по вакансиям; \n"
-            "4 - Список всех вакансий, у которых зарплата выше средней по всем вакансиям; \n"
-            "5 - Список всех вакансий по ключевому слову; \n"
-            "6 - Вывести полный список вакансий из json-файла; \n"
-            "7 - Создать новые таблицы в БД и загрузить данные из csv-файла; \n"
+            "1 - Вывести полный список вакансий из json-файла; \n"
+            "2 - Создать новые таблицы в БД и загрузить данные из csv-файла; \n"
+            "3 - Список всех компаний и количество вакансий у каждой компании; \n"
+            "4 - Список всех вакансий с указанием названия компании, названия вакансии, зарплаты и ссылки; \n"
+            "5 - Средняя зарплата по вакансиям; \n"
+            "6 - Список всех вакансий, у которых зарплата выше средней по всем вакансиям; \n"
+            "7 - Список всех вакансий по ключевому слову; \n"
             "exit - Выйти. \n"
         )
         if command.lower() == 'exit':
             break
 
         elif command.lower() == '1':
-            print(db.get_companies_and_vacancies_count())
-
-        elif command.lower() == '2':
-            print(db.get_all_vacancies())
-
-        elif command.lower() == '3':
-            print(db.get_avg_salary())
-
-        elif command.lower() == '4':
-            print(db.get_vacancies_with_higher_salary())
-
-        elif command.lower() == '5':
-            keyword = input('Введите ключевое слово для поиска: ').lower()
-            print(db.get_vacancies_with_keyword(keyword))
-
-        elif command.lower() == '6':
             vacancies = connector.select()
             for vacancy in vacancies:
                 print(vacancy, end='\n\n')
 
-        elif command.lower() == '7':
+        elif command.lower() == '2':
             DBManager.create_tables_with_data()
             print('Таблицы с актуальными данными созданы.')
+
+        elif command.lower() == '3':
+            print(db.get_companies_and_vacancies_count())
+
+        elif command.lower() == '4':
+            print(db.get_all_vacancies())
+
+        elif command.lower() == '5':
+            print(db.get_avg_salary())
+
+        elif command.lower() == '6':
+            print(db.get_vacancies_with_higher_salary())
+
+        elif command.lower() == '7':
+            keyword = input('Введите ключевое слово для поиска: ').lower()
+            print(db.get_vacancies_with_keyword(keyword))
+
         else:
             print('Введена неверная команда.')
 
